49520f6abbcf0a3f82ca9bb905a77aa48efbcf11
Hadwin <linnaiwei@gmail.com>
2020-04-20 06:04:44 +0800

    Add bulma to project and create a component MusicNav

---
+++ src/renderer/components/IndexPage/Layout/MusicNav.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="breadcrumb has-bullet-separator" aria-label="breadcrumbs">
+        <ul>
+            <li><a href="#">Bulma</a></li>
+            <li><a href="#">Documentation</a></li>
+            <li><a href="#">Components</a></li>
+            <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "MusicNav"
+    }
+</script>
+
+<style scoped>
+
+</style>
